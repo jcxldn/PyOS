a23e447bf85376d54c5b00c36d8d426bf189194e
Prouser123 <jcahill773@gmail.com>
2018-10-22 16:19:56 +0100

    Partial fix for #17

--- internal/update/runscript.py
+++ internal/update/runscript.py
@@ -26,8 +26,12 @@ def app():
 		ver = ver[1:]
 	print("Installed Version: " + internal.extra.colors.WARNING + internal.extra.notes.ver + internal.extra.colors.ENDC)
 	print("Newest Version: " + internal.extra.colors.WARNING + ver + internal.extra.colors.ENDC)
-	if (float(internal.extra.notes.ver) >= float(ver)):
-		print("You are up-to-date!")
-	else:
-		print("There is a new version available.")
+	try:
+		if (float(internal.extra.notes.ver) >= float(ver)):
+			print("You are up-to-date!")
+		else:
+			print("There is a new version available.")
+			print("To update please visit: https://github.com/Prouser123/PyOS/releases/latest")
+	except Exception as e:
+		print("Unable to verify if you are up-to-date.")
 		print("To update please visit: https://github.com/Prouser123/PyOS/releases/latest")
