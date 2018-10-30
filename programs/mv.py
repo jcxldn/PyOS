e7401038cadbf826199e94b086a666e93936a6f6
Ra Inta <ra@carter.carter.com>
2018-10-29 20:46:09 -0500

    Cleaned up some aspects of cp and mv that violated DRY principles

--- programs/mv.py
+++ programs/mv.py
@@ -6,31 +6,20 @@
 # PyOS Scripts
 import internal.extra
 import os
+from cp import displayCwdFiles, getFileOrigin
+
 
 def app():
     print(internal.extra.colors.OKGREEN + "Moving (renaming) files: " + internal.extra.colors.ENDC)
     print(internal.extra.colors.BOLD + "File to move/rename (enter filename or number of file): " + internal.extra.colors.ENDC)
-    file_list = os.listdir(os.getcwd())
-    # Make a list of all files in current folder
-    for Idx, file_name in enumerate(file_list):
-        print("| {0} | {1} ".format(Idx, file_name))
-    def getfileOrigin():
-        """Get input for file to be copied, and validate that it exists in the current folder (or filesystem)."""
-        origin_file = raw_input("[0-" + str(len(file_list)) + "] > ")
-        # Check to see if input was a valid number
-        if origin_file.isdigit() and (int(origin_file) >= 0) and (int(origin_file) <= len(file_list) - 1):
-                origin_file = file_list[int(origin_file)]
-        # Default file to copy is first element of the list
-        if origin_file == "":
-            origin_file = file_list[0]
-        return origin_file
-    origin_file = getfileOrigin()
+    file_list = displayCwdFiles()
+    origin_file = getFileOrigin(file_list)
     # Validate chosen file to move
     while not os.path.isfile(origin_file):
-        print(internal.extra.colors.BOLD + "Warning! file " + origin_file + " does not yet exist.\n\n"  + internal.extra.colors.ENDC)
-        origin_file = getfileOrigin()
+        print(internal.extra.colors.BOLD + "Warning! file " + origin_file + " does not yet exist.\n\n" + internal.extra.colors.ENDC)
+        origin_file = getFileOrigin(file_list)
     print(internal.extra.colors.BOLD + origin_file + " selected for moving." + internal.extra.colors.ENDC)
-    target_file = raw_input(internal.extra.colors.BOLD + "Enter filename to move to [Enter to backup]:"  + internal.extra.colors.ENDC)
+    target_file = raw_input(internal.extra.colors.BOLD + "Enter filename to move to [Enter to backup]:" + internal.extra.colors.ENDC)
     if target_file == "":
         target_file = origin_file + ".bak"
     if os.path.isfile(target_file):
