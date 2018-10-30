e8cbe3cd6279feb54ca7f223dfb46ce32f8c32d3
Deepanshu Sinha <107sinha@gmail.com>
2018-10-30 13:48:01 +0530

    changed sytax to support python2

--- programs/snakes_ladders.py
+++ programs/snakes_ladders.py
@@ -1,4 +1,3 @@
-from PIL import Image
 import random
 
 end = 100
@@ -6,28 +5,28 @@ end = 100
 
 def check_ladder(points):
     if points == 8:
-        print('Ladder')
+        print 'Ladder'
         return 26
     elif points == 21:
-        print('Ladder')
+        print 'Ladder'
         return 82
     elif points == 43:
-        print('Ladder')
+        print 'Ladder'
         return 77
     elif points == 50:
-        print('Ladder')
+        print 'Ladder'
         return 91
     elif points == 54:
-        print('Ladder')
+        print 'Ladder'
         return 93
     elif points == 62:
-        print('Ladder')
+        print 'Ladder'
         return 96
     elif points == 66:
-        print('Ladder')
+        print 'Ladder'
         return 87
     elif points == 80:
-        print('Ladder')
+        print 'Ladder'
         return 100
     else:
         # Not a ladder
@@ -36,43 +35,43 @@ def check_ladder(points):
 
 def check_snake(points):
     if points == 44:
-        print('Snake')
+        print 'Snake'
         return 22
     elif points == 46:
-        print('Snake')
+        print 'Snake'
         return 5
     elif points == 48:
-        print('Snake')
+        print 'Snake'
         return 9
     elif points == 52:
-        print('Snake')
+        print 'Snake'
         return 11
     elif points == 55:
-        print('Snake')
+        print 'Snake'
         return 7
     elif points == 59:
-        print('Snake')
+        print 'Snake'
         return 17
     elif points == 64:
-        print('Snake')
+        print 'Snake'
         return 36
     elif points == 69:
-        print('Snake')
+        print 'Snake'
         return 33
     elif points == 73:
-        print('Snake')
+        print 'Snake'
         return 1
     elif points == 83:
-        print('Snake')
+        print 'Snake'
         return 19
     elif points == 92:
-        print('Snake')
+        print 'Snake'
         return 51
     elif points == 95:
-        print('Snake')
+        print 'Snake'
         return 24
     elif points == 98:
-        print('Snake')
+        print 'Snake'
         return 28
     else:
         # Not a snake
@@ -88,9 +87,9 @@ def reached_end(points):
 
 def play():
     # Input player 1 name
-    p1_name = input('Player 1, Please enter your name: ')
+    p1_name = raw_input('Player 1, Please enter your name: ')
     # Input player 2 name
-    p2_name = input('Player 2, Please enter your name: ')
+    p2_name = raw_input('Player 2, Please enter your name: ')
     # Initialise points of player 1
     pp1 = 0
     # Initialise points of player 2
@@ -101,17 +100,17 @@ def play():
     while(1):
         if turn % 2 == 0:
             # Player 1 turn
-            print(p1_name, ' your turn')
+            print p1_name + ' your turn'
             # Ask player's choice to continue
-            c = input('Press 1 to continue, 0 to quit: ')
-            if c == 0:
-                print(p1_name, ' scored ', pp1)
-                print(p2_name, ' scored ', pp2)
-                print('Quitting the game, Thanks for playing')
+            c = raw_input('Press 1 to continue, 0 to quit: ')
+            if int(c) == 0:
+                print p1_name + ' scored ' + str(pp1)
+                print p2_name + ' scored ' + str(pp2)
+                print 'Quitting the game, Thanks for playing'
                 break
             # Generate a random number from 1, 2, 3, 4, 5, 6
             dice = random.randint(1, 6)
-            print('Dice showed: ', dice)
+            print 'Dice showed: ' + str(dice)
             # Add points
             pp1 = pp1 + dice
             pp1 = check_ladder(pp1)
@@ -121,24 +120,24 @@ def play():
             if pp1 > end:
                 pp1 = end
 
-            print(p1_name, ' Your score: ', pp1)
+            print p1_name + ' Your score: ' + str(pp1)
 
             if reached_end(pp1):
-                print(p1_name, 'won')
+                print p1_name + ' won'
                 break
         else:
             # Player 2 turn
-            print(p2_name, ' your turn')
+            print p2_name + ' your turn'
             # Ask player's choice to continue
-            c = input('Press 1 to continue, 0 to quit: ')
-            if c == 0:
-                print(p1_name, ' scored ', pp1)
-                print(p2_name, ' scored ', pp2)
-                print('Quitting the game, Thanks for playing')
+            c = raw_input('Press 1 to continue, 0 to quit: ')
+            if int(c) == 0:
+                print p1_name + ' scored ' + str(pp1)
+                print p2_name + ' scored ' + str(pp2)
+                print 'Quitting the game, Thanks for playing'
                 break
             # Generate a random number from 1, 2, 3, 4, 5, 6
             dice = random.randint(1, 6)
-            print('Dice showed: ', dice)
+            print 'Dice showed: ' + str(dice)
             # Add points
             pp2 = pp2 + dice
             pp2 = check_ladder(pp2)
@@ -148,10 +147,10 @@ def play():
             if pp2 > end:
                 pp2 = end
 
-            print(p2_name, ' Your score: ', pp2)
+            print p2_name + ' Your score: ' + str(pp2)
 
             if reached_end(pp2):
-                print(p2_name, 'won')
+                print p2_name + ' won'
                 break
 
         turn = turn + 1
