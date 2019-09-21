89ffef3a77472197f9e7cfac346bd9f42b325c8e
James Cahill <jcahill773@gmail.com>
2019-09-21 20:00:12 +0100

    bump version

--- internal/extra.py
+++ internal/extra.py
@@ -10,7 +10,7 @@ import os
 
 class notes:
     name = ("PyOS")
-    ver = ("2.2-base-master")
+    ver = ("3.0-base-master")
     author = ("Prouser123")
     helpmsg = ("Type 'help' for a list of commands.")
 
